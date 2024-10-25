50c63b4a88496148ed5ba5cf21358b820df4d1f6
Pene891 <pene1086@gmail.com>
2024-10-25 16:12:37 +0200

    components1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PokemonCard from './components/PokemonCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div>
+      <PokemonCard />
+  </div>
+
   )
 }
 
-export default App
+export default PokemonCard
