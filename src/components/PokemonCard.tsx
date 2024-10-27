412ba4e3e3af48de978edb30b238a5999cff6f37
Pene891 <pene1086@gmail.com>
2024-10-27 15:53:03 +0100

    interfaceHipokemon

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,26 +1,21 @@
-function PokemonCard() {
-  const pokemon = pokemonList[1];
+interface Hipokemon {
+  name: string;
+  imgSrc?: string;
+}
+
+function PokemonCard({ pokemon }: Hipokemon) {
+  //const pokemon = pokemonList[1];
+  // console.log (pokemon)
   return (
     <figure>
-      {pokemon.imgSrc ? (
-        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      {pokemon[0].imgSrc ? (
+        <img src={pokemon[0].imgSrc} alt={pokemon[0].name} />
       ) : (
         <p>???</p>
       )}
-      <figcaption>{pokemon.name}</figcaption>
+      <figcaption>{pokemon[0].name}</figcaption>
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
